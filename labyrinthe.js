3b7383110e32edc030de45b1afbbee31b65433c4
Steve <stevebotella@hotmail.fr>
2021-07-06 11:47:08 +0200

    feat: win condition

--- labyrinthe.js
+++ labyrinthe.js
@@ -7,15 +7,11 @@ let labyrinth = [
     ["o", "x", "o", "o", "o", "x", "o"],
 ]
 let location = [0, 0]
-let endLocation = [2, 4]
+let win = false
 let xIndex = 0
 let yIndex = 0
-t = 0
 
-// Display value console.log()
-
-// remplacer locatuion par trouver G
-while ((location[0] != endLocation[0]) || (location[1] != endLocation[1])) {
+while (!win) {
     move()
     console.table(labyrinth)
 }
@@ -23,6 +19,13 @@ console.log("Win bro' !")
 
 function move() {
     if ((xIndex < labyrinth.length - 1) && (yIndex < labyrinth[1].length - 1)) {
+        if (labyrinth[location[0]][location[1] - 1] == "G") {
+            win = true
+            labyrinth[location[0]][location[1]] = "v"
+            location = [location[0], location[1] - 1]
+            labyrinth[location[0]][location[1]] = "S"
+            yIndex--
+        }
         if (labyrinth[location[0] + 1][location[1]] == "o") {
             labyrinth[location[0]][location[1]] = "v"
             location = [location[0] + 1, location[1]]
@@ -33,7 +36,7 @@ function move() {
             location = [location[0], location[1] + 1]
             labyrinth[location[0]][location[1]] = "S"
             yIndex++
-        } else if ((labyrinth[location[0]][location[1] - 1] == "o") || (labyrinth[location[0]][location[1] - 1] == "G")) {
+        } else if ((labyrinth[location[0]][location[1] - 1] == "o")) {
             labyrinth[location[0]][location[1]] = "v"
             location = [location[0], location[1] - 1]
             labyrinth[location[0]][location[1]] = "S"
@@ -48,7 +51,7 @@ function move() {
             location = [location[0] - 1, location[1]]
             labyrinth[location[0]][location[1]] = "S"
             xIndex--
-        }else if (labyrinth[location[0]][location[1] + 1] == "v") {
+        } else if (labyrinth[location[0]][location[1] + 1] == "v") {
             labyrinth[location[0]][location[1]] = "v"
             location = [location[0], location[1] + 1]
             labyrinth[location[0]][location[1]] = "S"
@@ -69,7 +72,7 @@ function move() {
             labyrinth[location[0]][location[1]] = "S"
             xIndex++
         }
-    } else if ((xIndex == labyrinth.length - 1)) {
+    } else if ((xIndex == labyrinth.length - 1) || (yIndex == labyrinth[1].length - 1)) {
         if (labyrinth[location[0]][location[1] + 1] == "o") {
             labyrinth[location[0]][location[1]] = "v"
             location = [location[0], location[1] + 1]
@@ -111,84 +114,5 @@ function move() {
             labyrinth[location[0]][location[1]] = "S"
             xIndex++
         }
-    } else if ((yIndex == labyrinth[1].length - 1)) {
-        if (labyrinth[location[0]][location[1] + 1] == "o") {
-            labyrinth[location[0]][location[1]] = "v"
-            location = [location[0], location[1] + 1]
-            labyrinth[location[0]][location[1]] = "S"
-            yIndex++
-        } else if (labyrinth[location[0]][location[1] - 1] == "o") {
-            labyrinth[location[0]][location[1]] = "v"
-            location = [location[0], location[1] - 1]
-            labyrinth[location[0]][location[1]] = "S"
-            yIndex--
-        } else if (labyrinth[location[0] - 1][location[1]] == "o") {
-            labyrinth[location[0]][location[1]] = "v"
-            location = [location[0] - 1, location[1]]
-            labyrinth[location[0]][location[1]] = "S"
-            xIndex--
-        } else if (labyrinth[location[0] - 1][location[1]] == "v") {
-            labyrinth[location[0]][location[1]] = "v"
-            location = [location[0] - 1, location[1]]
-            labyrinth[location[0]][location[1]] = "S"
-            xIndex--
-        } else if (labyrinth[location[0]][location[1] + 1] == "v") {
-            labyrinth[location[0]][location[1]] = "v"
-            location = [location[0], location[1] + 1]
-            labyrinth[location[0]][location[1]] = "S"
-            yIndex++
-        } else if (labyrinth[location[0]][location[1] - 1] == "v") {
-            labyrinth[location[0]][location[1]] = "v"
-            location = [location[0], location[1] - 1]
-            labyrinth[location[0]][location[1]] = "S"
-            yIndex--
-        } else if (labyrinth[location[0] - 1][location[1]] == "v") {
-            labyrinth[location[0]][location[1]] = "v"
-            location = [location[0] - 1, location[1]]
-            labyrinth[location[0]][location[1]] = "S"
-            xIndex--
-        } else if (labyrinth[location[0] + 1][location[1]] == "o") {
-            labyrinth[location[0]][location[1]] = "v"
-            location = [location[0] + 1, location[1]]
-            labyrinth[location[0]][location[1]] = "S"
-            xIndex++
-        }
-    }
-}
-
-//Test
-function checkBot(xLoc, yLoc) {
-    if (labyrinth[xLoc + 1][yLoc] == "o") {
-        labyrinth[xLoc][yLoc] = "v"
-        location = [xLoc + 1, yLoc]
-        labyrinth[xLoc][yLoc] = "S"
-        xIndex++
-    }
-}
-
-function checkTop(xLoc, yLoc) {
-    if (labyrinth[xLoc - 1][yLoc] == "o") {
-        labyrinth[xLoc][yLoc] = "v"
-        location = [xLoc - 1, yLoc]
-        labyrinth[xLoc][yLoc] = "S"
-        xIndex--
-    }
-}
-
-function checkRgt(xLoc, yLoc) {
-    if (labyrinth[xLoc][yLoc + 1] == "o") {
-        labyrinth[xLoc][yLoc] = "v"
-        location = [xLoc, yLoc + 1]
-        labyrinth[xLoc][yLoc] = "S"
-        yIndex++
-    }
-}
-
-function checkLft(xLoc, yLoc) {
-    if (labyrinth[xLoc][yLoc - 1] == "o") {
-        labyrinth[xLoc][yLoc] = "v"
-        location = [xLoc, yLoc - 1]
-        labyrinth[xLoc][yLoc] = "S"
-        yIndex--
     }
 }
